8d1a303506543ae5a7c7dfd41100ed21544cf4db
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-06-29 02:26:10 -0400

    6

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rails db:seed
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake db:migrate
+bundle exec rails db:seed
 
-chmod a+x bin/render-build.sh
