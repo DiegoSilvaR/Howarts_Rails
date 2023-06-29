92baeeecb76690d7b6f52c46dad2279e72989084
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-06-29 00:34:25 -0400

    primer commit

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rails db:seed
+bundle exec rake db:migrate
+
+chmod a+x bin/render-build.sh
